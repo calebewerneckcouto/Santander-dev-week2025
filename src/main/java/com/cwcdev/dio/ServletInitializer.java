e163f80377dbf8cefd2d028f8afea1524f75dd97
calebewerneckcouto <calebewerneck@hotmail.com>
2025-04-03 14:59:27 -0300

    criacao do projeto java usando gradle

---
+++ src/main/java/com/cwcdev/dio/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cwcdev.dio;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SantanderDevWeek2025Application.class);
+	}
+
+}
---
+++ src/test/java/com/cwcdev/dio/SantanderDevWeek2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwcdev.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeek2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
